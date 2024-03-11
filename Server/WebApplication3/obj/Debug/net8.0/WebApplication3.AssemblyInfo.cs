c7e1a28c503ee5a5af62be080eed21d26ea77626
Kyos22 <nguyenthanhcongvt123@gmail.com>
2024-03-11 19:19:07 +0700

    update

--- Server/WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
+++ Server/WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a66ec99054202ea3a04d7574fdb57bfc4ff6b13")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
