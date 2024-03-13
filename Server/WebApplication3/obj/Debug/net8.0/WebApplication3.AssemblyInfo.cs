a8ee707b360da39a3da44296ffede2d105cc177b
tranp6648 <tranp6648@gmail.com>
2024-03-13 17:52:55 +0700

    update

--- Server/WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
+++ Server/WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9dff793f6f071b0372b34a2596e1d97a5950ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4d43b74a4009607ad4136ceef265d66af47de5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
